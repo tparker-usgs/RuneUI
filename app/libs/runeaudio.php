f2ad8246ef9b565866ceb61484ce137ba8e080e4
Unknown <ys3al35l@gmail.com>
2018-03-18 06:58:12 -0500

    Removed shairport
    Replaced shairport with shairport-sync in UI

--- app/libs/runeaudio.php
+++ app/libs/runeaudio.php
@@ -1922,7 +1922,9 @@ function wrk_cleanDistro($redis)
     // reset libao config file
     sysCmd('cp /var/www/app/config/defaults/libao.conf /etc/libao.conf');
     // reset shairport starter script
-    sysCmd('cp /var/www/app/config/defaults/shairport.service /usr/lib/systemd/system/shairport.service');
+    sysCmd('cp /var/www/app/config/defaults/shairport-sync.service /usr/lib/systemd/system/shairport-sync.service');
+    // reset shairport config file
+    sysCmd('cp /var/www/app/config/defaults/shairport-sync.conf /etc/shairport-sync.conf');
     // reset spop config file
     sysCmd('cp /var/www/app/config/defaults/spopd.conf /etc/spop/spopd.conf');
     // reset mpdscribble config file
@@ -2530,10 +2532,14 @@ function wrk_shairport($redis, $ao, $name = null)
     }
     $acard = json_decode($redis->hget('acards', $ao));
     runelog('acard details: ', $acard);
-    $file = '/usr/lib/systemd/system/shairport.service';
-    $newArray = wrk_replaceTextLine($file, '', 'ExecStart=', 'ExecStart=/usr/bin/shairport -w --name="'.$name.'" --on-start=$ON --on-stop=$OFF --meta-dir=/var/run/shairport -o alsa -- -d plug'.$acard->device);
-    runelog('shairport.service :', $newArray);
-    // Commit changes to /usr/lib/systemd/system/shairport.service
+    $file = '/usr/lib/systemd/system/shairport-sync.service';
+    //$newArray = wrk_replaceTextLine($file, '', 'ExecStart=', 'ExecStart=/usr/bin/shairport-sync -c /etc/shairport-sync.conf --name="'.$name.'" -- -d '.$acard->mixer_device.' -c '.$acard->mixer_control);
+	if ($acard->mixer_control != '')
+		$newArray = wrk_replaceTextLine($file, '', 'ExecStart=', 'ExecStart=/usr/bin/shairport-sync -c /etc/shairport-sync.conf --name="'.$name.'" -- -d '.$acard->mixer_device.' -c '.$acard->mixer_control);
+	else
+		$newArray = wrk_replaceTextLine($file, '', 'ExecStart=', 'ExecStart=/usr/bin/shairport-sync -c /etc/shairport-sync.conf --name="'.$name.'" -- -d '.$acard->device);
+    runelog('shairport-sync.service :', $newArray);
+    // Commit changes to /usr/lib/systemd/system/shairport-sync.service
     $fp = fopen($file, 'w');
     fwrite($fp, implode("", $newArray));
     fclose($fp);
@@ -2551,8 +2557,8 @@ function wrk_shairport($redis, $ao, $name = null)
     // update systemd
     sysCmd('systemctl daemon-reload');
     if ($redis->hGet('airplay','enable') === '1') {
-        runelog('restart shairport');
-        sysCmd('systemctl restart shairport');
+        runelog('restart shairport-sync');
+        sysCmd('systemctl restart shairport-sync');
     }
     // set process priority
     sysCmdAsync('sleep 1 && rune_prio nice');
@@ -3095,7 +3101,7 @@ function wrk_changeHostname($redis, $newhostname)
     // update airplayname
     if ($redis->hGet('airplay','name') === $hn[0]) {
         $redis->hSet('airplay','name', $newhostname);
-        if ($redis->hGet('airplay','enable') === '1') sysCmd('systemctl restart shairport');
+        if ($redis->hGet('airplay','enable') === '1') sysCmd('systemctl restart shairport-sync');
     }
     // update AVAHI serice data
     wrk_avahiconfig($newhostname);
