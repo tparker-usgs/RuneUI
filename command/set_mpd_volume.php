b72a12198b94384b96372b6556ec3fc1d285d42c
janui <janui@onmat.com>
2018-07-28 02:03:47 +0200

    0.5b: Auto stash before merge of "0.5b" and "origin/0.5b"

---
+++ command/set_mpd_volume.php
@@ -0,0 +1,119 @@
+#!/usr/bin/php
+<?php
+/*
+ * Copyright (C) 2013-2014 RuneAudio Team
+ * http://www.runeaudio.com
+ *
+ * RuneUI
+ * copyright (C) 2013-2014 - Andrea Coiutti (aka ACX) & Simone De Gregori (aka Orion)
+ *
+ * RuneOS
+ * copyright (C) 2013-2014 - Simone De Gregori (aka Orion) & Carmelo San Giovanni (aka Um3ggh1U)
+ *
+ * RuneAudio website and logo
+ * copyright (C) 2013-2014 - ACX webdesign (Andrea Coiutti)
+ *
+ * This Program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This Program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RuneAudio; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.txt>.
+ *
+ *  file: command/set_mpd_volume
+ *  version: 1.5
+ *  date: 27 July 2018
+ *  coder: janui
+ *
+ */
+// common include
+ini_set('error_log', '/var/log/set_mpd_volume.log');
+define('APP', '/srv/http/app/');
+include('/srv/http/app/libs/runeaudio.php');
+// Connect to Redis backend
+$redis = new Redis();
+$redis->connect('/run/redis.sock');
+// reset logfile
+sysCmd('echo "--------------- start: set_mpd_volume.php ---------------" > /var/log/runeaudio/set_mpd_volume.log');
+runelog('WORKER set_mpd_volume.php STARTING...');
+
+$activePlayer = $redis->get('activePlayer');
+if ($activePlayer === 'MPD') {
+	// Start MPD (if  not started) in order to set the startup volume (if needed and if set) then kill MPD (if required)
+	$retval = sysCmd('systemctl is-active mpd');
+	if ($retval[0] === 'active') {
+		// do nothing
+	} else {
+		sysCmd('systemctl start mpd');
+	}
+	unset($retval);
+	$mpdstartvolume = $redis->get('mpd_start_volume');
+	// sometimes mpd fails to start correctly (e.g. when the incorrect audio card overlay is specified)
+	// then it will never return a volume value so limit the number of retries with a counter
+	$retries_volume = 40;
+	if ($redis->hGet('mpdconf', 'mixer_type') != 'disabled') {
+		if ($mpdstartvolume != -1) {
+			do {
+				sleep(2);
+				// retry until MPD is up and returns the any valid volume value
+				$retval = sysCmd('mpc volume | grep "volume:" | cut -d ":" -f 2 | cut -d "%" -f 1');
+				$initvolume = trim($retval[0]);
+				unset($retval);
+				if ($initvolume == $mpdstartvolume) {
+					// no need to do anything, volume is correct
+					$mpdvolume = $mpdstartvolume;
+				} else if ($initvolume == 'n/a') {
+					// something wrong, mismatch between redis and mpd 'disabled' values
+					$retries_volume = 0;
+				} else {
+					// set the mpd volume
+					$retval = sysCmd('mpc volume '.$mpdstartvolume.' | grep "volume:" | cut -d ":" -f 2 | cut -d "%" -f 1');
+					$mpdvolume = trim($retval[0]);
+				}
+				unset($retval);
+			} while (($mpdstartvolume != $mpdvolume) && (--$retries_volume > 0));
+			// When MPD is killed the state (including the volume) is saved
+			if ($initvolume != $mpdvolume) {
+				// kill mpd if the volume was actually changed
+				sysCmd('mpd --kill');
+			}
+		} else {
+			do {
+				sleep(2);
+				// retry until MPD is up and returns the any valid volume value
+				$retval = sysCmd('mpc volume | grep "volume:" | cut -d ":" -f 2 | cut -d "%" -f 1');
+				$mpdvolume = trim($retval[0]);
+				unset($retval);
+			} while ((!is_numeric($mpdvolume)) && (--$retries_volume > 0));
+		}
+	} else {
+		// mixer_type is disabled so set volume variable to 100%
+		$mpdvolume = 100;
+	}
+	// Save the last known MPD volume
+	if ($retries_volume > 0) {
+		// correctly set or disabled
+		$redis->set('lastmpdvolume', $mpdvolume);
+	} else if ($mpdstartvolume != -1) {
+		// failed to set it, but start volume has a value
+		$redis->set('lastmpdvolume', $mpdstartvolume);
+	} else {
+		// set it to 40% when we don't have a value
+		$redis->set('lastmpdvolume', 40);
+	}
+	// restart mpd if required
+	$retval = sysCmd('systemctl is-active mpd');
+	if ($retval[0] === 'active') {
+		// do nothing
+	} else {
+		sysCmd('systemctl start mpd');
+	}
+	unset($retval);
+}
