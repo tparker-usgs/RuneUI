656e3aff7ba545f4910a6142c7f74ae9ba0653df
janui <janui@onmat.com>
2018-08-23 16:48:05 +0200

    New defailt chromium startup file

---
+++ app/config/defaults/start_chromium.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+CHROMIUM_TEMP=/tmp/chromium
+sudo http mkdir -p $CHROMIUM_TEMP
+export HOME=$CHROMIUM_TEMP
+
+chromium \
+--app=http://localhost \ 
+--start-fullscreen \
+--force-device-scale-factor=1.8
