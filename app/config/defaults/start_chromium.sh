42e54015fac28d7ff6dcaad57dbb21a810565c4a
janui <janui@onmat.com>
2018-08-23 17:01:12 +0200

    Wip/stash fix

--- app/config/defaults/start_chromium.sh
+++ app/config/defaults/start_chromium.sh
@@ -5,11 +5,4 @@ CHROMIUM_TEMP=/tmp/chromium
 sudo http mkdir -p $CHROMIUM_TEMP
 export HOME=$CHROMIUM_TEMP
 
-<<<<<<< Updated upstream
-chromium \
---app=http://localhost \ 
---start-fullscreen \
---force-device-scale-factor=1.8
-=======
 chromium --app=http://localhost --start-fullscreen --force-device-scale-factor=1.8
->>>>>>> Stashed changes
