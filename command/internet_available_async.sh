04f0a631d13eb24640cfc08c7bb75b78dda1022a
janui <janui@onmat.com>
2019-02-02 19:13:57 +0100

    Fix for testing internet connection, eliminates hanging up the UI

--- command/internet_available_async.sh
+++ command/internet_available_async.sh
@@ -4,11 +4,21 @@
 # ------------------------
 # Test to see if an internet connection is available. Disable the artistinfo & lyrics functions when no internet is available
 # This will resolve UI freezing problems
+#
+# see if we can see google.com, this command will give up after +/-20 seconds (= timeout x retries)
 wget --force-html --spider --timeout=10 --tries=2 https://www.google.com/ > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-  echo success
+#	internet connection is available, so make the artist info & lyric files executable
+	chown http.http /srv/http/command/lyric.sh
+	chown http.http /srv/http/command/artist_info.sh
+	chmod 755 /srv/http/command/lyric.sh
+	chmod 755 /srv/http/command/artist_info.sh
 else
-  echo failed
+#	internet connection not available, so make the artist info & lyric files non-executable
+	chown root.root /srv/http/command/lyric.sh
+	chown root.root /srv/http/command/artist_info.sh
+	chmod 600 /srv/http/command/lyric.sh
+	chmod 600 /srv/http/command/artist_info.sh
 fi
 #---
 #End script
