c193c3c89eb17791dc3eadf0b65284e275ab03b2
janui <janui@onmat.com>
2019-02-04 16:31:58 +0100

    Fix for UI problems caused by the lyrics service going off line

--- command/internet_available_async.sh
+++ command/internet_available_async.sh
@@ -9,9 +9,9 @@
 wget --force-html --spider --timeout=10 --tries=2 https://www.google.com/ > /dev/null 2>&1
 if [ $? -eq 0 ]; then
 #	internet connection is available, so make the artist info & lyric files executable
-	chown http.http /srv/http/command/lyric.sh
+	chown root.root /srv/http/command/lyric.sh
 	chown http.http /srv/http/command/artist_info.sh
-	chmod 755 /srv/http/command/lyric.sh
+	chmod 600 /srv/http/command/lyric.sh
 	chmod 755 /srv/http/command/artist_info.sh
 else
 #	internet connection not available, so make the artist info & lyric files non-executable
