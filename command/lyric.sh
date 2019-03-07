7346dc9108d11b5a82e2e3b1d2d68370a0ddafb1
janui <janui@onmat.com>
2019-03-07 15:01:06 +0100

    Include timeouts and limited retries for bash curl commands

--- command/lyric.sh
+++ command/lyric.sh
@@ -9,7 +9,7 @@ title=`perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$title"`
 echo $artist
 echo $title
 
-lyric=$( curl -sf "https://makeitpersonal.co/lyrics?artist=$artist&title=$title" | sed ':a;N;$!ba;s/\n/<\/br>/g' | xargs -0 )
+lyric=$( curl -s -f --connect-timeout 5 -m 10 --retry 2 "https://makeitpersonal.co/lyrics?artist=$artist&title=$title" | sed ':a;N;$!ba;s/\n/<\/br>/g' | xargs -0 )
 
 if [[ $lyric == *"We're sorry, but something went wrong."* ]];
 then
