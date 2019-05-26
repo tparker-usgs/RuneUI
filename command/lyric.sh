75d8a0047348f638d3e1dac2795999b1cc0f9c41
janui <janui@onmat.com>
2019-05-26 16:20:59 +0200

    Fix last.fm outage problem

--- command/lyric.sh
+++ command/lyric.sh
@@ -9,7 +9,7 @@ title=`perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$title"`
 echo $artist
 echo $title
 
-lyric=$( curl -s -f --connect-timeout 5 -m 10 --retry 2 "https://makeitpersonal.co/lyrics?artist=$artist&title=$title" | sed ':a;N;$!ba;s/\n/<\/br>/g' | xargs -0 )
+lyric=$( curl -s -f --connect-timeout 1 -m 10 --retry 2 "https://makeitpersonal.co/lyrics?artist=$artist&title=$title" | sed ':a;N;$!ba;s/\n/<\/br>/g' | xargs -0 )
 
 if [[ $lyric == *"something went wrong"* ]];
 then
