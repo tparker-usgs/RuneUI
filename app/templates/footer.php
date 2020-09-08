109341400d3437ab1074d533b74032ad869c7b63
janui <janui@onmat.com>
2020-09-08 18:36:18 +0200

    Reverting back to jquery 2.2.4

--- app/templates/footer.php
+++ app/templates/footer.php
@@ -22,7 +22,7 @@
 </div>
 <!-- loader -->
 <div id="loader"<?php if ($this->section == 'dev') { ?> class="hide"<?php } ?>><div id="loaderbg"></div><div id="loadercontent"><i class="fa fa-refresh fa-spin"></i>connecting...</div></div>
-<script src="<?=$this->asset('/js/vendor/jquery-3.5.1.min.js')?>"></script>
+<script src="<?=$this->asset('/js/vendor/jquery-2.2.4.min.js')?>"></script>
 <script src="<?=$this->asset('/js/vendor/pushstream.min.js')?>"></script>
 <script src="<?=$this->asset('/js/vendor/bootstrap.min.js')?>"></script>
 <?php if ($this->section == 'debug'): ?>
