991997da81943bc0940bbde0f7bd1c07cb5ba966
janui <janui@onmat.com>
2020-10-10 16:50:51 +0200

    Optimisations to reduce nic up/down logging

--- command/internet_available_async.sh
+++ command/internet_available_async.sh
@@ -9,24 +9,33 @@
 set +e # continue on errors
 #
 # just in case something has gone wrong with the local router link, try to reconnect
-# if a nic is down, use IP to take the nic down manually and bring it up
+# if a Wi-Fi nic is down, use IP to take the nic down manually and bring it up
 # connman will then reconnect automatically
-# get a list of the nics which are down
-NICS=$(ip -o -br  address | grep -i 'down' | cut -d ' ' -f1 | xargs)
-for NIC in $NICS
-do
-    ip link set dev $NIC down
-    ip link set dev $NIC up
-done
 #
 # if connman has lost a Wi-Fi connection it should reconnect automatically, but the current version does not do it
 # running 'iwctl station <nic> scan' for the Wi-Fi nics should initiate a reconnect
 # no real issue in running this every time this routine runs
+# get a list of the nics which are down
+DOWN=$(ip -o -br  address | grep -i 'down' | cut -d ' ' -f1 | xargs)
 # get a list of all Wi-Fi nics
 NICS=$(iw dev | grep -i interface | cut -d ' ' -f2 | xargs)
 for NIC in $NICS
 do
-    # scan for wireless networks per nic
+    # only for Wi-Fi nics
+    if [[ "$DOWN" =~ "$NIC" ]]; then
+        # only for nics which are down
+        if [ -f "/tmp/$NIC.up" ]; then
+            # only for nics which were previously up
+            ip link set dev $NIC down
+            ip link set dev $NIC up
+        fi
+    else
+        # nic is up
+        # create a file '/tmp/<nic name>.up' for each Wi-Fi interface which is up
+        # the /tmp directory is a TMPFS file-system which will be recreated on reboot
+        touch /tmp/$NIC.up
+    fi
+    # scan for wireless networks per wireless nic
     iwctl station $NIC scan
 done
 #
