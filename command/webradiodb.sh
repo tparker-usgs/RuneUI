e2afbd25be13839874c6eee4feb1215aa43ab528
anybody <any@body.com>
2020-11-23 15:30:57 +0100

    Cleaned up files and added .xbindkeysrc to .gitignore

--- command/webradiodb.sh
+++ command/webradiodb.sh
@@ -7,16 +7,16 @@
 
 # if sub directories
 if ls -d /mnt/MPD/Webradio/*/ &> /dev/null; then
-	# -mindepth 2 = in sub directories && -type f = file
-	find /mnt/MPD/Webradio -mindepth 2 -type f -name *.pls -exec cp -f -- '{}' /mnt/MPD/Webradio \;
-	# * = all sub directory && .[^.] = not ..
-	rm -rf /mnt/MPD/Webradio/{*,.[^.]}/
+    # -mindepth 2 = in sub directories && -type f = file
+    find /mnt/MPD/Webradio -mindepth 2 -type f -name *.pls -exec cp -f -- '{}' /mnt/MPD/Webradio \;
+    # * = all sub directory && .[^.] = not ..
+    rm -rf /mnt/MPD/Webradio/{*,.[^.]}/
 fi
 
 if ! ls /mnt/MPD/Webradio/*.pls &> /dev/null; then
-	title -l '=' "$info No webradio files found."
-	title -nt 'Copy *.pls to /mnt/MPD/Webradio/ then run again.'
-	exit
+    title -l '=' "$info No webradio files found."
+    title -nt 'Copy *.pls to /mnt/MPD/Webradio/ then run again.'
+    exit
 fi
 
 #title -l '=' "$bar Webradio Import ..."
@@ -26,12 +26,12 @@ redis-cli del webradios &> /dev/null
 
 # add data from files
 for file in /mnt/MPD/Webradio/*.pls; do
-	name=$( basename "$file" )
-	name=${name%.*}
-	url=$( grep 'File1' "$file" | cut -d '=' -f2 )
-	
-	redis-cli hset webradios "$name" "$url" &> /dev/null
-	printf "%-30s : $url\n" "$name"
+    name=$( basename "$file" )
+    name=${name%.*}
+    url=$( grep 'File1' "$file" | cut -d '=' -f2 )
+
+    redis-cli hset webradios "$name" "$url" &> /dev/null
+    printf "%-30s : $url\n" "$name"
 done
 
 # refresh list
