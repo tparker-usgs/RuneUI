06e677456c5be7c995c5ff56fa658945681e4028
janui <janui@onmat.com>
2020-12-19 13:59:36 +0100

    Create /usr/share/X11/xorg.conf.d/99-raspi-rotate.conf on installation

--- command/raspi-rotate-install.sh
+++ command/raspi-rotate-install.sh
@@ -7,7 +7,7 @@ mkdir /home/raspi-rotate
 cd /home/raspi-rotate
 #
 # save the current versions of the distribution files
-cp /var/www/app/config/defaults/99-raspi-rotate.conf.tmpl /home/raspi-rotate/99-raspi-rotate.conf.tmpl
+cp /var/www/app/config/defaults/99-raspi-rotate.conf.* /home/raspi-rotate/
 cp /var/www/app/config/defaults/runeaudio-0.5-bootsplashes.tar.gz /home/raspi-rotate/runeaudio-0.5-bootsplashes.tar.gz
 cp /var/www/command/01-bootsplash.sh /home/raspi-rotate/01-bootsplash.sh
 cp /var/www/command/raspi-rotate-screen.sh /home/raspi-rotate/raspi-rotate-screen.sh
@@ -23,7 +23,7 @@ chmod  755 /usr/share/doc/raspi-rotate
 chmod  644 /usr/share/doc/raspi-rotate/*
 #
 # copy the current versions of the distribution back to their normal places
-cp /home/raspi-rotate/99-raspi-rotate.conf.tmpl /var/www/app/config/defaults/99-raspi-rotate.conf.tmpl
+cp /home/raspi-rotate/99-raspi-rotate.conf.* /var/www/app/config/defaults/
 cp /home/raspi-rotate/runeaudio-0.5-bootsplashes.tar.gz /var/www/app/config/defaults/runeaudio-0.5-bootsplashes.tar.gz
 cp /home/raspi-rotate/01-bootsplash.sh /var/www/command/01-bootsplash.sh
 cp /home/raspi-rotate/raspi-rotate-screen.sh /var/www/command/raspi-rotate-screen.sh
@@ -46,3 +46,6 @@ mv -n /usr/share/bootsplash /usr/share/bootsplash-save
 #
 # set the screen orientation
 /srv/http/command/01-bootsplash.sh NORMAL
+#
+# copy the config file
+cp /var/www/app/config/defaults/99-raspi-rotate.conf.tmpl /usr/share/X11/xorg.conf.d/99-raspi-rotate.conf
